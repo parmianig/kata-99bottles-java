56f3998943fe503be3e4af798edfe68417820b53
Primo Giuseppe Parmiani <giuseppe.parmiani@consultant.aruba.it>
2023-11-04 11:36:09 +0100

    add 99 bottles test

--- src/main/java/Song99Bottles.java
+++ src/main/java/Song99Bottles.java
@@ -1,5 +1,10 @@
+import java.util.ArrayList;
+
 public class Song99Bottles {
-    public boolean handle() {
-        return true;
+    public ArrayList<String> sing() {
+        String text = "99 bottles of beer on the wall, 99 bottles of beer. Take one down and pass it around, 98 bottles of beer on the wall.\n";
+        ArrayList<String> lyrics = new ArrayList<>();
+        lyrics.add(text);
+        return lyrics;
     }
 }
--- src/test/java/Song99BottlesTest.java
+++ src/test/java/Song99BottlesTest.java
@@ -1,14 +1,21 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertTrue;
 
 public class Song99BottlesTest {
     @Test
-    @DisplayName("Add true is true")
-    void add() {
+    @DisplayName("Sing 99 bottles")
+    void shouldSing99Bottles() {
         Song99Bottles song99Bottles = new Song99Bottles();
-        assertTrue(song99Bottles.handle());
+        
+        ArrayList<String> handle = song99Bottles.sing();
+        
+        final String expected = "99 bottles of beer on the wall, 99 bottles of beer. Take one down and pass it around, 98 bottles of beer on the wall.\n";
+        assertEquals(1, handle.size());
+        assertEquals(expected, handle.getFirst());
     }
 }
